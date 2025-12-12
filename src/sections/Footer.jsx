c8250d051091ed81a4127ca75e55e0acc172f3e3
Gian Y. Anday <giancarloanday@gmail.com>
2025-12-12 10:57:54 +0800

    Updated all files with the latest changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,27 +1,33 @@
 import { socialImgs } from "../constants";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-container">
-        <div className="flex flex-col justify-center">
-          <p>Terms & Conditions</p>
-        </div>
-        <div className="socials">
-          {socialImgs.map((socialImg, index) => (
-            <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="flex flex-col justify-center">
+                    <p>Terms & Conditions</p>
+                </div>
+                <div className="socials">
+                    {socialImgs.map((img, i) => (
+                        <a
+                            className="icon"
+                            target="_blank"
+                            href={img.url}
+                            key={i}
+                            rel="noopener noreferrer"
+                        >
+                            <img src={img.imgPath} alt={`Social media link to ${img.name}`} />
+                        </a>
+                    ))}
+                </div>
+                <div className="flex flex-col justify-center">
+                    <p className="text-center md:text-end">
+                        © {new Date().getFullYear()} Gian Y. Anday. All rights reserved.
+                    </p>
+                </div>
             </div>
-          ))}
-        </div>
-        <div className="flex flex-col justify-center">
-          <p className="text-center md:text-end">
-            © {new Date().getFullYear()} Adrian Hajdin. All rights reserved.
-          </p>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 };
 
 export default Footer;
--- src/sections/LogoShowcase.jsx
+++ src/sections/LogoShowcase.jsx
@@ -1,30 +1,35 @@
 import { logoIconsList } from "../constants";
+import TitleHeader from "../components/TitleHeader.jsx";
 
 const LogoIcon = ({ icon }) => {
-  return (
-    <div className="flex-none flex-center marquee-item">
-      <img src={icon.imgPath} alt={icon.name} />
-    </div>
-  );
+    return (
+        <div className="flex-none flex-center marquee-item">
+            <img src={icon.imgPath} alt={icon.name} />
+        </div>
+    );
 };
 
 const LogoShowcase = () => (
-  <div className="md:my-20 my-10 relative">
-    <div className="gradient-edge" />
-    <div className="gradient-edge" />
+    <div className="md:my-20 my-10 relative">
+        <TitleHeader
+            title="The tools I'm working with..."
+            sub="🔨 My Toolkit"
+        />
+        <div className="gradient-edge" />
+        <div className="gradient-edge" />
 
-    <div className="marquee h-52">
-      <div className="marquee-box md:gap-12 gap-5">
-        {logoIconsList.map((icon, index) => (
-          <LogoIcon key={index} icon={icon} />
-        ))}
+        <div className="marquee h-52">
+            <div className="marquee-box md:gap-12 gap-5">
+                {logoIconsList.map((icon, index) => (
+                    <LogoIcon key={index} icon={icon} />
+                ))}
 
-        {logoIconsList.map((icon, index) => (
-          <LogoIcon key={index} icon={icon} />
-        ))}
-      </div>
+                {logoIconsList.map((icon, index) => (
+                    <LogoIcon key={index} icon={icon} />
+                ))}
+            </div>
+        </div>
     </div>
-  </div>
 );
 
 export default LogoShowcase;
